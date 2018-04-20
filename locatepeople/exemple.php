1979cc521520a52fdfcf6254250ad7ef91436d90
Baptiste Beugnet <beugnet@intechinfo.fr>
2018-04-20 15:43:57 +0200

    localisation amélioré

--- locatepeople/exemple.php
+++ locatepeople/exemple.php
@@ -13,6 +13,29 @@ echo $record->postal_code . "\n";
 echo $record->latitude . "\n";
 echo $record->longitude . "\n";
 
+$la = $record->latitude;
+$lo = $record->longitude;
+
+$url = "http://maps.google.com/maps/geo?output=csv&q=".$la.",".$lo;
+
+if($csv = file_get_contents($url))
+{
+   if(substr($csv,0,3)!=200)
+   {
+      die("Erreur");
+   }
+   else
+   {
+      $adresse = substr($csv, 7, -1);
+      echo $adresse;
+   }
+}
+else
+{
+   echo "Erreur";
+}
+
+
 geoip_close($gi);
 
 ?>
