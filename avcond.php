9029db6f7523162fc1f9ddb09609dc6c6c5682e4
Baptiste Beugnet <beugnet@intechinfo.fr>
2018-04-23 18:53:58 +0200

    Avis

--- avcond.php
+++ avcond.php
@@ -1,1 +1,50 @@
-avcond.php
+<center><table style="border: 2px solid white">
+	<tr>
+		<td style="color: yellow;border: solid white">Conducteur</td>
+		<td style="color: yellow;border: solid white">Avis</td>
+		<td style="color: yellow;border: solid white">Note</td>
+	</tr>
+
+	<?php
+	
+$nbcomm = $databaseConnection->prepare('SELECT COUNT(*) FROM avicond WHERE pseudo_iencli = :login');
+			$nbcomm->bindParam(':login', $login);
+			$nbcomm->execute();
+			$nb = $nbcomm->fetch();
+			$nbc = $nb[0];
+
+			if ($nbc==0)
+				{
+				echo "Vous n'avez pas encore d'avis";
+				}
+			else
+				{
+				echo "Vous avez " ,$nbc, " avis <br>";
+				// On recupere tout le contenu de la table news
+				$reponse = $databaseConnection->prepare('SELECT id, pseudo_iencli, pseudo_cond, Avis, Note FROM avicond WHERE pseudo-iencli = :login');
+  				$reponse->bindParam(':login', $login);
+  				$reponse->execute();
+				while ($donnees = $reponse->fetch())
+				{
+    //On affiche les données dans le tableau
+				$Note += $donnees['Note'];
+
+ ?>
+                <tr>
+                    <td style="color: white;border: solid white"><?php echo $donnees['pseudo_cond'];?></td>
+                    <td style="color: white;border: solid white"><?php echo $donnees['Avis'];?></td>
+                    <td style="color: white;border: solid white"><?php echo $donnees['Note'];?></td>
+                </tr>
+
+<?php 
+
+}
+$moy = $note/$nbc;
+echo "Vous avez ",$moy," de note moyenne"; 
+$reponse->closeCursor();
+}
+
+?>
+</table>
+</body>
+</html>
