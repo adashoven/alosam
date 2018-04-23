2cfa12df0e3ade613e16c1aa3df48836c9a1161a
Baptiste Beugnet <beugnet@intechinfo.fr>
2018-04-23 15:30:11 +0200

    parametre 2

--- param.php
+++ param.php
@@ -30,6 +30,95 @@
                     </tr>
                 </table>
             </center>
+        <div class="portfolio-modal modal fade" id="infpers" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Info Perso</h2>
+<?php
+include('db.php');
+            $login = $_SESSION['login'];
+    try {
+        $databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
+        $databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+            $records = $databaseConnection->prepare('SELECT id,nom,prenom,login,pass, mail FROM  test WHERE login = :login');
+            $records->bindParam(':login',$login );
+            $records->execute();
+            $results = $records->fetch(PDO::FETCH_ASSOC);
+            $nom = $results['nom'];
+            $prenom = $results['prenom'];   
+ ?>
+<center>
+    <table>
+        <tr>
+            <td> nom </td>
+            <td> <?php echo $nom;?></td>
+        </tr>
+        <tr>
+            <td> prenom </td>
+            <td> <?php echo $prenom;?></td>
+        </tr>
+    </table>
+</center>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+                <div class="portfolio-modal modal fade" id="contconf" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Info Perso</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+                <div class="portfolio-modal modal fade" id="avicond" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Info Perso</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
