b3c114994e0fe79be00dd36a2cf4b139214c7134
Baptiste Beugnet <beugnet@intechinfo.fr>
2018-04-24 15:51:44 +0200

    recuperation

--- addadrss.php
+++ addadrss.php
@@ -1,8 +1,10 @@
 <?php 
 if ($_POST['dom'] == 'Non'){
 	echo 'ajouter ladresse mais pas en dom';
+	echo $_POST['adrs'];
 }
 else if ($_POST['dom'] == 'Oui'){
 	echo 'ajouter ladresse en dom';
+	echo $_POST['adrs'];
 }
 ?>
