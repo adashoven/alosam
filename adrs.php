dc3dfd9048c7d79a7954bc13352b5df5f5dc5c2b
Baptiste Beugnet <beugnet@intechinfo.fr>
2018-04-24 16:49:21 +0200

    affichage des adresses

--- adrs.php
+++ adrs.php
@@ -1,1 +1,23 @@
 <a href="addadress.php">vous souhaitez ajouter une adresse ? cliquez ici</a>
+<p> Voila vos adresses connu </p>
+
+ <?php           
+ $records = $databaseConnection->prepare('SELECT iencli_adrss,dom FROM  adrsusers WHERE login = :login');
+            $records->bindParam(':login',$login );
+            $records->execute();
+            $results = $records->fetch(PDO::FETCH_ASSOC);
+            $ia = $results['iencli_adrss'];
+            $dom = $results['dom'];
+
+    ?>
+    <table>
+        <tr>
+            <td><p> Adresse </p></td>
+            <td><p> <?php echo $ia;?> </p></td>
+        </tr>        
+        <tr>
+        	
+            <td><p> Domicile ? </p></td>
+            <td><p> <?php echo $dom;?> </p></td>
+        </tr>
+    </table>
